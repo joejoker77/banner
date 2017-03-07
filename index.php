cd575e907a4636547ee4c7d018222303e6796ce5
Vitaliy Yanovskiy <joejoker77@gmail.com>
2017-03-07 19:09:08 +0300

    Init

---
+++ index.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body style="margin: 0;padding: 0;background: #ccc">
+<div class="cbh-reset-style-widget cbh_popup_holder" id="cbh_popup_holder">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="/js/masked.js"></script>
+    <div class="callbackhunterOverlay" id="callbackhunterOverlay">
+        <span class="callbackhunterOverlay_text" id="callbackhunterOverlay_text">Спасибо, ждите звонка</span>
+        <br>
+    </div>
+    <div class="cbh_popup_wrapper" id="cbh_popup_wrapper">
+        <div class="cbh_image_show_holder" id="cbh_image_show_holder">
+            <img class="cbh_image_show" id="cbh_image_show" src="/images/banner.jpg">
+            <div class="cbh_text1_show" id="cbh_text1_show">
+                <h3 style="text-align: right;">
+                    <span class="cbh_text1_span" id="cbh_text_1"></span>
+                </h3>
+            </div>
+        </div>
+        <div class="disclaimer_blocbh_bottom">
+            <label id="disclaimer_for_advertising" for="cbh_pd_check">
+                <input type="checkbox" id="chbox_2" checked=""><span></span>
+                <a href="" target="_blank"></a>
+            </label>
+            <br>
+            <label id="disclaimer_for_basis" class="" for="cbh_pd_checbh_double">
+                <input type="checkbox" id="chbox" checked=""><span></span>
+                <a href="" target="_blank"></a>
+            </label>
+        </div>
+        <aside class="cbh_text_holder" id="cbh_text_holder">
+            <div class="cbh_aside_container" id="cbh_aside_container">
+                <div class="nsp" id="nsp">
+                    <div class="cbh_net_spasibo" id="cbh_net_spasibo">Нет, спасибо</div>
+                    <div class="cbh_lg_exit_container" id="cbh_lg_exit_container">
+                        <svg id="cbh_exit_button_close" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16"><path id="x_exit_lg" d="M15.854 12.854c-0-0-0-0-0-0l-4.854-4.854 4.854-4.854c0-0 0-0 0-0 0.052-0.052 0.090-0.113 0.114-0.178 0.066-0.178 0.028-0.386-0.114-0.529l-2.293-2.293c-0.143-0.143-0.351-0.181-0.529-0.114-0.065 0.024-0.126 0.062-0.178 0.114 0 0-0 0-0 0l-4.854 4.854-4.854-4.854c-0-0-0-0-0-0-0.052-0.052-0.113-0.090-0.178-0.114-0.178-0.066-0.386-0.029-0.529 0.114l-2.293 2.293c-0.143 0.143-0.181 0.351-0.114 0.529 0.024 0.065 0.062 0.126 0.114 0.178 0 0 0 0 0 0l4.854 4.854-4.854 4.854c-0 0-0 0-0 0-0.052 0.052-0.090 0.113-0.114 0.178-0.066 0.178-0.029 0.386 0.114 0.529l2.293 2.293c0.143 0.143 0.351 0.181 0.529 0.114 0.065-0.024 0.126-0.062 0.178-0.114 0-0 0-0 0-0l4.854-4.854 4.854 4.854c0 0 0 0 0 0 0.052 0.052 0.113 0.090 0.178 0.114 0.178 0.066 0.386 0.029 0.529-0.114l2.293-2.293c0.143-0.143 0.181-0.351 0.114-0.529-0.024-0.065-0.062-0.126-0.114-0.178z" style="fill: rgb(0, 0, 0);"></path></svg>
+                    </div>
+                </div>
+                <div class="cbh_text2_show">
+                    <h5 class="cbh_text2" id="cbh_text_2">
+                        <br>Рекордная выгода <b><br>до 250 000 руб. + ПОДАРКИ!</b>
+                        <br>Спешите! Количество автомобилей по акции ограничено.</h5>
+                </div>
+                <div class="cbh_cd" id="cbh_cd">
+                    <div class="cbh_countdown_text">
+                        <h6 id="cbh_countdown_text">До конца акции осталось:</h6>
+                    </div>
+                    <div id="cbh_countdown" class="cbh_countdown">
+                        <span class="days">00<span class="note">дней</span></span>
+                        <span class="hours">00<span class="note">часов</span></span>
+                        <span class="minutes">00<span class="note">минут</span></span>
+                        <span class="seconds">00<span class="note">секунд</span></span>
+                    </div>
+                </div>
+                <div class="cbh_input_form" id="cbh_input_form">
+                    <div class="cbh_form-group">
+                        <select name="cbh_input_dropdown" class="cbh_input_dropdown" id="cbh_input_dropdown">
+                            <option value="0">Выберите подразделение</option>
+                            <option value="0">0</option>
+                        </select>
+                        <h6 class="cbh_enter_number" id="cbh_phoneNumberField">Пожалуйста, введите номер</h6>
+                        <input type="text" name="cbh_phone_number" class="ckStandartStyle cbh_input_email" id="cbh_text1" placeholder="+7 ( ___ ) ___ - ____" maxlength="16">
+                    </div>
+                    <button type="submit" name="submit" class="ckStandartStyle cbh_submit_frontpage" id="cbh_buttonCall">
+                        <span>Хорошо, жду звонка</span>
+                    </button>
+                    <br>
+                    <div class="cbh_checkbox_div" id="cbh_checkbox_div">
+                        <div class="cbh_wish" id="cbh_wish_text_small"></div>
+                    </div>
+                </div>
+                <br>
+                <label class="cbh_serviceDelivered ckService" id="serviceProvided">Сервис предоставлен
+                    <a href="https://www.callbackhunter.com" target="_blank">CallbackHunter</a>
+                </label>
+            </div>
+        </aside>
+    </div>
+    <script type="text/javascript" src="http://cbh.loc/cbh-debug.js?hunter_code=61fce64ce47c297c5b17dbadc2067abb&platform=<?=$platform?>" charset="UTF-8"></script>
+    <script type="text/javascript">
+        function timeToEvent(eventDate){
+            var now = new Date(),
+                daystoED    = Math.floor(Math.round(eventDate-now)/86400000),
+                hourstoED   = 24 - now.getHours() - 1,
+                minutestoED = 60 - now.getMinutes() - 1,
+                secondstoED = 60 - now.getSeconds() - 1;
+
+            daystoED    = (daystoED < 1) ? "0"+daystoED : daystoED;
+            hourstoED   = (hourstoED < 10) ? "0"+hourstoED : hourstoED;
+            minutestoED = (minutestoED < 10) ? "0"+minutestoED : minutestoED;
+            secondstoED = (secondstoED < 10) ? "0"+secondstoED : secondstoED;
+
+            return '<span class="days">' + daystoED + '<span class="note">дней</span></span>' +
+                '<span class="hours">' + hourstoED + '<span class="note">часов</span></span>' +
+                '<span class="minutes">' + minutestoED + '<span class="note">минут</span></span>' +
+                '<span class="seconds">' + secondstoED + '<span class="note">секунд</span></span>';
+        }
+        var CBHSettings = { showWidgetEnter: 'myCBHEnter', showWidgetExit: 'myCBHExit' };
+        window.myCBHEnter = function(){
+            document.getElementById('cbh_popup_holder').classList.add('opened');
+        };
+        window.myCBHExit = function(){
+            document.getElementById('cbh_popup_holder').classList.remove('opened');
+        };
+        window.onload = function(){
+
+            var eventDate = new Date(2017,2,31),
+                count     = document.getElementById('cbh_countdown'),
+                textDiv   = document.getElementById('cbh_countdown_text');
+
+            textDiv.nodeValue = "До окончания распродажи осталось ";
+            document.getElementById('cbh_cd').style.display = 'block';
+
+            new Masked({selector: '#cbh_text1', country: 'ru'});
+
+            var cbhInterval     = window.setInterval(function(){
+                count.innerHTML = timeToEvent(eventDate);
+            },1000);
+
+            if(new Date() > eventDate){
+                clearInterval(cbhInterval);
+            }
+        };
+    </script>
+</div>
+</body>
+</html>
